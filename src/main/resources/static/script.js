c3551e0eec70d7d3b7a0df9d312b5a6b27b10841
nityapisolkar <pisolkarnitya@gmail.com>
2025-06-13 12:20:12 -0700

    wrestling wtih frontend i hate this so much

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,41 @@
+let addCountIndex = 0;
+function addFnIndex() {
+    if (addCountIndex === 0) {
+        const inputFields = document.getElementById("inputFields");
+
+        if (!inputFields.classList.contains("has-inputs")) {
+            inputFields.classList.add("has-inputs");
+          }
+
+        inputFields.innerHTML += `
+        <div class="input-container">
+            <input type = "date" placeholder = "Start Date (YYYY-MM-DD)" class = "input-field short">
+            <input type = "text" placeholder = "Duration (days)" class = "input-field short">
+            <input type = "text" placeholder = "Symptoms: mood, acne, bloating, etc." class = "input-field full">
+            <button class = "plus-button">+</button>
+        </div>
+    `;
+    addCountIndex++;
+    }
+}
+
+let addCountPeriods = 0;
+function addFnPeriods() {
+    if (addCountPeriods === 0) {
+        const inputFields = document.getElementById("inputFields");
+
+        if (!inputFields.classList.contains("has-inputs")) {
+            inputFields.classList.add("has-inputs");
+          }
+
+        inputFields.innerHTML += `
+        <div class="input-container period-input-container">
+            <input type = "date" placeholder = "Start Date (YYYY-MM-DD)" class = "input-field short">
+            <input type = "text" placeholder = "Duration (days)" class = "input-field short">
+            <input type = "text" placeholder = "Symptoms: mood, acne, bloating, etc." class = "input-field full">
+        </div>
+    `;
+    addCountPeriods++;
+    }
+}
+
