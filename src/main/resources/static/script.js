79845917e193b7048b1a1a6a7c6f0ceb986f3d62
nityapisolkar <pisolkarnitya@gmail.com>
2025-06-18 22:42:47 -0700

    cleaned up frontend ugh (next: make stuff from input actually display in table)

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -7,14 +7,24 @@ function addFnIndex() {
             inputFields.classList.add("has-inputs");
           }
 
-        inputFields.innerHTML += `
-        <div class="input-container">
-            <input type = "date" placeholder = "Start Date (YYYY-MM-DD)" class = "input-field short">
-            <input type = "text" placeholder = "Duration (days)" class = "input-field short">
-            <input type = "text" placeholder = "Symptoms: mood, acne, bloating, etc." class = "input-field full">
-            <button class = "plus-button">+</button>
-        </div>
-    `;
+    //     inputFields.innerHTML += `
+    //     <div class="input-container">
+    //         <input type = "date" placeholder = "Start Date (YYYY-MM-DD)" class = "input-field short">
+    //         <input type = "text" placeholder = "Duration (days)" class = "input-field short">
+    //         <input type = "text" placeholder = "Symptoms: mood, acne, bloating, etc." class = "input-field full">
+    //         <button class = "plus-button">+</button>
+    //     </div>
+    // `;
+            inputFields.innerHTML += `
+            <div class="input-container">
+            <div class="input-row">
+                <input type="date" placeholder="Start Date (YYYY-MM-DD)" class="input-field short">
+                <input type="text" placeholder="Duration (days)" class="input-field short">
+            </div>
+            <input type="text" placeholder="Symptoms: mood, acne, bloating, etc." class="input-field full" maxlength="150">
+            <button class="plus-button">+</button>
+            </div>
+        `;
     addCountIndex++;
     }
 }
@@ -32,7 +42,7 @@ function addFnPeriods() {
         <div class="input-container period-input-container">
             <input type = "date" placeholder = "Start Date (YYYY-MM-DD)" class = "input-field short">
             <input type = "text" placeholder = "Duration (days)" class = "input-field short">
-            <input type = "text" placeholder = "Symptoms: mood, acne, bloating, etc." class = "input-field full">
+            <input type = "text" placeholder = "Symptoms: mood, acne, bloating, etc." class = "input-field full" maxlength="150">
         </div>
     `;
     addCountPeriods++;
